e4a2a59be7523d90691b921c1ecbb78c19a934fa
sugerme <hanakazu8989@gmail.com>
2019-12-26 12:02:19 +0900

    first init

---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+.container
+  div
+    logo
+      h1 2019toyohashi
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+<style lang="stylus">
+.container
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+
+.title
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+</style>
