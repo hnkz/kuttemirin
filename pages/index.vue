41ca0efa13b5ac42d5ea6988e9a5a07268275c19
root <root@localhost.localdomain>
2019-12-26 22:28:18 +0000

    toppage design and images

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,16 @@
 <template lang="pug">
 .container
-  .main
-    header
-      h3 くってみりん
-    .contents
-      v-btn(color="#B86513" dark flat depressed class="top-btn" to="recommend" ) お店を探す
-    footer
+  .contents
+    .text
+      v-card(color="rgba(204, 167, 159, 0.3)" dark flat depressed)
+        v-card-title
+          p
+          | くってみりん
+          br
+          | ~豊橋でモーニングを楽しもう~
+    .btn
+      v-btn(color="#67391C" dark flat depressed class="top-btn" to="recommend" ) お店を探す
+  footer
 </template>
 
 <script>
@@ -15,21 +20,28 @@ export default {
   }
 }
 </script>
+
 <style scoped lang="stylus">
 .container
-  margin: 0 auto;
-  min-height: 100vh;
-.main
-  padding: 15px;
-header
-  height: 150px;
-  text-align: center;
+  background: linear-gradient( rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3) ) ,url("~assets/images/bg.jpg");
+  background-size: cover;
+  background-blend-mode: saturation;
+  background-repeat: no-repeat;
+  background-position: center;
 .contents
+  margin-top: 10vh;
+  text-align: center;
+.text
+  display: inline-flex;
   text-align: center;
 .top-btn
   height: 10vh!important
   width: 30vh!important
+  font-weight: bold;
   max-width: 100%;
   max-height 100%;
-  props: color;
+  text-align: center;
+  margin-top: 15vh;
+.btn
+  text-align: center;
 </style>
